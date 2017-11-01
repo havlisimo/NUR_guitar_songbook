14f1ec9937b2a8799c8186405480953966a47d2f
Tomas Havlicek <tomas.havlicek@quanti.cz>
2017-11-01 11:26:30 +0100

    initial commit

---
+++ app/src/main/java/cz/cvut/fit/nurguitarsongbook/MainActivity.kt
@@ -0,0 +1,12 @@
+package cz.cvut.fit.nurguitarsongbook
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
