ee142005df010dac294380ce39e653c9eee533fb
Kosut Ondrej <ext-ondrej.kosut@trimble.com>
2017-11-24 19:18:04 +0200

    SongDetail: Add song edit activity and connect it to edit button in song

--- app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/songdetail/SongEditActivity.kt
+++ app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/songdetail/SongEditActivity.kt
@@ -4,56 +4,45 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import cz.cvut.fit.nurguitarsongbook.App
 import cz.cvut.fit.nurguitarsongbook.R
 import cz.cvut.fit.nurguitarsongbook.model.entity.Song
-
 import kotlinx.android.synthetic.main.activity_song_detail.*
-import cz.cvut.fit.nurguitarsongbook.R.string.app_name
-import cz.cvut.fit.nurguitarsongbook.main.chord.ChordListFragment
+
+import kotlinx.android.synthetic.main.activity_song_edit.*
 import kotlinx.android.synthetic.main.content_song_detail.*
+import kotlinx.android.synthetic.main.content_song_edit.*
 import kotlinx.android.synthetic.main.fragment_song_detail.*
 
-
-class SongDetailActivity : AppCompatActivity() {
+class SongEditActivity : AppCompatActivity() {
 
     var song: Song = Song( 0, "", "", "" )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val songId = intent.getIntExtra(INTENT_SONG_ID, 0);
-        val songName = intent.getStringExtra(INTENT_SONG_NAME)
-        val songArtist = intent.getStringExtra(INTENT_SONG_ARTIST)
-        val songComment = intent.getStringExtra(INTENT_SONG_COMMENT)
+        setContentView(R.layout.activity_song_edit)
+        setSupportActionBar(toolbar_edit_song)
+        val songId = intent.getIntExtra(SongEditActivity.INTENT_SONG_ID, 0);
+        val songName = intent.getStringExtra(SongEditActivity.INTENT_SONG_NAME)
+        val songArtist = intent.getStringExtra(SongEditActivity.INTENT_SONG_ARTIST)
+        val songComment = intent.getStringExtra(SongEditActivity.INTENT_SONG_COMMENT)
         song = Song(songId, songName,songArtist,songComment)
-
-        setContentView(R.layout.activity_song_detail)
-        setSupportActionBar(toolbar)
-        supportActionBar!!.setTitle(song.name)
-        val f = fragment_song_detail
-        f.tv_song_name.setText( "Name: " + songName )
-        f.tv_song_artist.setText( "Artist: " + songArtist )
-        f.tv_song_comment.setText( "Comment:" + songComment )
+        this.et_song_name.setText( songName )
+        this.et_song_artist.setText( songArtist )
+        this.et_song_comment.setText( songComment )
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_song_detail, menu)
+        menuInflater.inflate(R.menu.menu_done, menu)
         return true
     }
 
-
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if (item?.itemId == R.id.action_edit_song) {
-            startActivity( SongEditActivity.newIntent( this, song ) );
+        if (item?.itemId == R.id.action_done) {
+            return true;
         }
         return super.onOptionsItemSelected(item)
     }
@@ -66,7 +55,7 @@ class SongDetailActivity : AppCompatActivity() {
         private val INTENT_SONG_COMMENT = "song_comment"
 
         fun newIntent(context: Context, song: Song): Intent {
-            val intent = Intent(context, SongDetailActivity::class.java)
+            val intent = Intent(context, SongEditActivity::class.java)
             intent.putExtra(INTENT_SONG_ID, song.id)
             intent.putExtra(INTENT_SONG_NAME, song.name)
             intent.putExtra(INTENT_SONG_ARTIST, song.artist)
