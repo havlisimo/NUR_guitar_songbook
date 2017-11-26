67e8fd54eb6e2c9809c3193a49a3292fe074be3a
Tomas Havlicek <tomas.havlicek@quanti.cz>
2017-11-26 12:04:46 +0100

    added home as up to song detail, changed its behaviour to

--- app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/song/songlist/SongListFragment.kt
+++ app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/song/songlist/SongListFragment.kt
@@ -1,10 +1,12 @@
 package cz.cvut.fit.nurguitarsongbook.main.song.songlist
 
 import android.os.Bundle
+import android.view.View
+import cz.cvut.fit.nurguitarsongbook.App
 import cz.cvut.fit.nurguitarsongbook.R
 import cz.cvut.fit.nurguitarsongbook.base.BaseAdapter
 import cz.cvut.fit.nurguitarsongbook.base.BaseListFragment
-import cz.cvut.fit.nurguitarsongbook.main.song.songdetail.SongDetailActivity
+import cz.cvut.fit.nurguitarsongbook.main.song.songdetail.SongDetailFragment
 import cz.cvut.fit.nurguitarsongbook.model.data.DataMockup
 import cz.cvut.fit.nurguitarsongbook.model.entity.Song
 import kotlinx.android.synthetic.main.item_song.view.*
@@ -27,6 +29,11 @@ class SongListFragment : BaseListFragment<Song>() {
         }
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        App.instance.activity?.setDisplayHomeAsUpEnabled(false)
+    }
+
     override fun getData(): MutableList<Song> {
         return mySongs
     }
@@ -38,7 +45,9 @@ class SongListFragment : BaseListFragment<Song>() {
         view.song_name.text = item.name
         view.song_artist.text = item.artist
         view.setOnClickListener {
-            startActivity( SongDetailActivity.newIntent( this.activity, DataMockup.songs.indexOf(item) ) )
+            App.instance.fragmentManager.changeFragment(SongDetailFragment::class.java,
+                SongDetailFragment::class.java.getSimpleName(),
+                SongDetailFragment.newDataBundle(DataMockup.songs.indexOf(item)))
         }
     }
 
