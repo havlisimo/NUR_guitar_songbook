78d99b783909c7a19433e9871fc588a821beb9ff
Tomas Havlicek <tomas.havlicek@quanti.cz>
2017-11-28 13:27:33 +0100

    song selection - delete selected or add selected to songbook functionality

--- app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/song/songlist/SongListFragment.kt
+++ app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/song/songlist/SongListFragment.kt
@@ -1,16 +1,26 @@
 package cz.cvut.fit.nurguitarsongbook.main.song.songlist
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.bignerdranch.android.multiselector.ModalMultiSelectorCallback
 import cz.cvut.fit.nurguitarsongbook.App
 import cz.cvut.fit.nurguitarsongbook.R
 import cz.cvut.fit.nurguitarsongbook.base.BaseSelectableListFragment
 import cz.cvut.fit.nurguitarsongbook.base.MultiselectAdapter
 import cz.cvut.fit.nurguitarsongbook.main.song.songdetail.SongDetailFragment
+import cz.cvut.fit.nurguitarsongbook.main.songbook.SongbookListFragment
 import cz.cvut.fit.nurguitarsongbook.model.data.DataMockup
 import cz.cvut.fit.nurguitarsongbook.model.entity.Song
 import kotlinx.android.synthetic.main.item_search_song_offline.view.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 
 /**
  * A placeholder fragment containing a simple view.
@@ -55,6 +65,16 @@ open class SongListFragment : BaseSelectableListFragment<Song>() {
         val view = holder!!.view
         view.songNameOffline.text = item.name
         view.songMetaOffline.text = item.artist
+
+        view.setOnLongClickListener( object : View.OnLongClickListener {
+            override fun onLongClick(v: View?): Boolean {
+                toggleSelection()
+                selector.setSelected(holder, true)
+                return true
+            }
+
+        })
+
         view.songbookLabels?.removeAllViews()
         if (item.songbooks.size ?: 0 > 0) {
             val containerWidth = App.convertDpToPixel(80f)
@@ -69,6 +89,47 @@ open class SongListFragment : BaseSelectableListFragment<Song>() {
                 view.songbookLabels?.addView(tv)
             }
         }
+    }
+
+    fun toggleSelection() {
+        //return if already selecting
+        if (selector.isSelectable) return
+        val a = activity as AppCompatActivity
+        a.startSupportActionMode(mDeleteMode)
+    }
+
+    val mDeleteMode = object : ModalMultiSelectorCallback(selector) {
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            when (item?.itemId) {
+                R.id.action_delete -> deleteSongs(mode)
+                R.id.action_add_song_to_list -> addToList(mode)
+            }
+            return true
+        }
+
+        override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+            super.onCreateActionMode(actionMode, menu)
+            activity.menuInflater.inflate(R.menu.menu_song_list, menu)
+            return true
+        }
+    }
+
+    private fun deleteSongs(mode: ActionMode?) {
+        alert(R.string.songbooks_delete_dialog) {
+            yesButton { adapter.deleteSelectedData()
+                longSnackbar(view, R.string.undo_songbook_deletion, R.string.undo, {adapter.undoDelete()})
+                mode!!.finish()
+            }
+            noButton {  }
+        }.show()
+    }
+
+    private fun addToList(mode: ActionMode?) {
+        mode!!.finish()
+        val bundle = Bundle()
+        bundle.putInt(SongbookListFragment.EXTRA_MODE, SongbookListFragment.MODE_SELECT_SINGLE)
+        bundle.putIntegerArrayList(SongbookListFragment.EXTRA_SELECTED_IDS, ArrayList(adapter.getSelectedItems().map { it.id }))
+        App.instance.fragmentManager.changeFragment(SongbookListFragment::class.java, SongbookListFragment::class.java.getSimpleName(), bundle)
     }
 
     companion object {
