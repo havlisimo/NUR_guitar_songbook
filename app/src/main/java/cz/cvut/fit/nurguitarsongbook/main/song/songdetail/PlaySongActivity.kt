c6f447235de693cda79169ce0b3003eac1232e99
Kosut Ondrej <ext-ondrej.kosut@trimble.com>
2017-11-28 16:36:51 +0100

    SongPlay: Add auto scroll

--- app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/song/songdetail/PlaySongActivity.kt
+++ app/src/main/java/cz/cvut/fit/nurguitarsongbook/main/song/songdetail/PlaySongActivity.kt
@@ -8,6 +8,7 @@ import cz.cvut.fit.nurguitarsongbook.R
 
 import kotlinx.android.synthetic.main.activity_play_song.*
 import kotlinx.android.synthetic.main.content_play_song.*
+import java.util.*
 
 class PlaySongActivity : AppCompatActivity() {
 
@@ -19,6 +20,20 @@ class PlaySongActivity : AppCompatActivity() {
         setSupportActionBar(toolbar_play_song)
         text = intent.getStringExtra( INTENT_SONG_TEXT )
         song_text.text = text
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        val autoUpdate = Timer()
+        autoUpdate.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread { updateHTML() }
+            }
+        }, 0, 25) // updates each 40 secs
+    }
+
+    private fun updateHTML() {
+        textAreaScroller.smoothScrollBy( 0, 1 )
     }
 
     companion object {
